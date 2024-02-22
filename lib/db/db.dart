c16759d5f4aa233edf00244a582604c6dd017067
alonsodesansegundo <alonsodesansegundo@gmail.com>
2024-02-22 19:56:52 +0100

    Añadidas nuevas rutinas a atención temprana, y creación del método getPreguntas

--- lib/db/db.dart
+++ lib/db/db.dart
@@ -1,8 +1,13 @@
 import 'package:path/path.dart';
+import 'package:rutinas/db/accion.dart';
+import 'package:rutinas/db/pregunta.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'grupo.dart';
 import 'jugador.dart';
+
+String pathPersonajes = "assets/img/personajes/";
+String pathAcciones = "assets/img/acciones/";
 
 Future<Database> initializeDB() async {
   String path = await getDatabasesPath();
@@ -10,9 +15,12 @@ Future<Database> initializeDB() async {
   return openDatabase(
     join(path, 'rutinas.db'),
     onCreate: (database, version) async {
-      createTables(database); // creación de tablas
-      insertGrupos(
-          database); // inserción de datos iniciales (grupos, preguntas...)
+      // creación de tablas
+      createTables(database);
+
+      // inserción de datos iniciales (grupos, preguntas...)
+      insertGrupos(database);
+      insertPreguntas(database);
     },
     version: 1,
   );
@@ -54,9 +62,7 @@ void createTablePregunta(Database database) {
       id INTEGER PRIMARY KEY AUTOINCREMENT, 
       enunciado TEXT NOT NULL,
       personajePath TEXT NOT NULL,
-      partidaId INTEGER,
       grupoId INTEGER,
-      FOREIGN KEY (partidaId) REFERENCES partida(id),
       FOREIGN KEY (grupoId) REFERENCES grupo(id))""");
 }
 
@@ -88,6 +94,146 @@ void insertGrupos(Database database) async {
     txn.rawInsert(
         "INSERT INTO grupo (nombre, edades) VALUES ('Adolescencia','12 - 17 años')");
   });
+}
+
+void insertPreguntas(Database database) {
+  insertPreguntasAccionesAtencionT(database);
+}
+
+// PREGUNTAS RUTINAS PARA EL GRUPO DE ATENCIÓN T.
+void insertPreguntasAccionesAtencionT(Database database) async {
+  int grupoAtencionT = 1;
+
+  // LAVAR DIENTES
+  int id_P1 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tiene que hacer Pepe para lavarse los dientes.',
+      'cerdo.png',
+      grupoAtencionT);
+  insertAccion(database, "Coger cepillo", 0, "1.LavarDientes.png", id_P1);
+  insertAccion(
+      database, "Echar pasta de dientes", 1, "2.LavarDientes.png", id_P1);
+  insertAccion(database, "Cepillarse", 2, "3.LavarDientes.png", id_P1);
+  insertAccion(database, "Lavar cepillo", 3, "4.LavarDientes.png", id_P1);
+  insertAccion(database, "Guardar cepillo", 4, "5.LavarDientes.png", id_P1);
+
+  // PEINARSE
+  int id_P2 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tendría que hacer Pepe si tuviera pelo y quisiera peinarse.',
+      'cerdo.png',
+      grupoAtencionT);
+  insertAccion(database, 'Coger el peine', 0, '6.CogerPeine.png', id_P2);
+  insertAccion(database, 'Peinarse', 1, '7.Peinarse.png', id_P2);
+  insertAccion(database, 'Guardar el peine', 2, '8.GuardarPeine.png', id_P2);
+
+  // LAVAR CARA
+  int id_P3 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tendría que hacer Pepe si quisiera lavarse la cara.',
+      'cerdo.png',
+      grupoAtencionT);
+
+  insertAccion(database, 'Abrir grifo', 0, '9.AbrirGrifo.png', id_P3);
+  insertAccion(database, 'Mojar manos', 1, '10.MojarManos.png', id_P3);
+  insertAccion(database, 'Cerrar grifo', 2, '11.CerrarGrifo.png', id_P3);
+  insertAccion(database, 'Lavar cara', 3, '12.LavarCara.png', id_P3);
+  insertAccion(database, 'Secar cara', 4, '13.SecarCara.png', id_P3);
+  insertAccion(database, 'Secar manos', 5, '14.SecarManos.png', id_P3);
+
+  // CALZARSE
+  int id_P4 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tendría que hacer Pepe si quisiera calzarse.',
+      'cerdo.png',
+      grupoAtencionT);
+  insertAccion(
+      database, 'Poner calcetines', 0, '15.PonerCalcetines.png', id_P4);
+  insertAccion(database, 'Poner calzado', 0, '16.PonerCalzado.png', id_P4);
+  insertAccion(database, 'Atar cordones', 0, '17.AtarCalzado.png', id_P4);
+}
+
+// PREGUNTAS RUTINAS PARA EL GRUPO DE LA INFANCIA
+void insertPreguntasAccionesInfancia(Database database) async {
+  int grupoInfancia = 2;
+
+  // LAVAR DIENTES
+  int id_P1 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tiene que hacer la fontanera María para lavarse los dientes.',
+      'fontanera.png',
+      grupoInfancia);
+  insertAccion(database, "Coger cepillo", 0, "1.LavarDientes.png", id_P1);
+  insertAccion(
+      database, "Echar pasta de dientes", 1, "2.LavarDientes.png", id_P1);
+  insertAccion(database, "Cepillarse", 2, "3.LavarDientes.png", id_P1);
+  insertAccion(database, "Lavar cepillo", 3, "4.LavarDientes.png", id_P1);
+  insertAccion(database, "Guardar cepillo", 4, "5.LavarDientes.png", id_P1);
+}
+
+// PREGUNTAS RUTINAS PARA EL GRUPO DE ADOLESCENTES
+void insertPreguntasAccionesAdolescencia(Database database) async {
+  int grupoAdolescencia = 2;
+
+  // LAVAR DIENTES
+  int id_P1 = await insertPregunta(
+      database,
+      'Por favor, pon en orden lo que tiene que hacer la futbolista Aitana para lavarse los dientes.',
+      'futbolista.png',
+      grupoAdolescencia);
+  insertAccion(database, "Coger cepillo", 0, "1.LavarDientes.png", id_P1);
+  insertAccion(
+      database, "Echar pasta de dientes", 1, "2.LavarDientes.png", id_P1);
+  insertAccion(database, "Cepillarse", 2, "3.LavarDientes.png", id_P1);
+  insertAccion(database, "Lavar cepillo", 3, "4.LavarDientes.png", id_P1);
+  insertAccion(database, "Guardar cepillo", 4, "5.LavarDientes.png", id_P1);
+}
+
+Future<int> insertPregunta(Database database, String enunciado,
+    String imgPersonaje, int grupoId) async {
+  int id = -1;
+  await database.transaction((txn) async {
+    id = await txn.rawInsert(
+      "INSERT INTO pregunta (enunciado, personajePath, grupoId) VALUES (?, ?, ?)",
+      [enunciado, pathPersonajes + imgPersonaje, grupoId],
+    );
+  });
+
+  return id;
+}
+
+Future<void> insertAccion(Database database, String texto, int orden,
+    String imgAccion, int preguntaId) async {
+  await database.transaction((txn) async {
+    await txn.rawInsert(
+      "INSERT INTO accion (texto, orden, imagenPath, preguntaId) VALUES (?, ?, ?, ?)",
+      [texto, orden, pathAcciones + imgAccion, preguntaId],
+    );
+  });
+}
+
+Future<List<Pregunta>> getPreguntas(int grupoId) async {
+  try {
+    final Database db = await initializeDB();
+    final List<Map<String, dynamic>> preguntasMap =
+        await db.query('pregunta', where: 'grupoId = ?', whereArgs: [grupoId]);
+    return preguntasMap.map((map) => Pregunta.preguntasFromMap(map)).toList();
+  } catch (e) {
+    print("Error al obtener preguntas: $e");
+    return [];
+  }
+}
+
+Future<List<Accion>> getAcciones(int preguntaId) async {
+  try {
+    final Database db = await initializeDB();
+    final List<Map<String, dynamic>> accionesMap = await db
+        .query('accion', where: 'preguntaId = ?', whereArgs: [preguntaId]);
+    return accionesMap.map((map) => Accion.accionesFromMap(map)).toList();
+  } catch (e) {
+    print("Error al obtener acciones: $e");
+    return [];
+  }
 }
 
 Future<List<Grupo>> getGrupos() async {
--- lib/db/pregunta.dart
+++ lib/db/pregunta.dart
@@ -3,35 +3,31 @@ class Pregunta {
   final String enunciado;
   final String personajePath;
   final int grupoId;
-  final int partidaId;
 
   Pregunta(
       {required this.id,
       required this.enunciado,
       required this.personajePath,
-      required this.grupoId,
-      required this.partidaId});
+      required this.grupoId});
 
   Pregunta.preguntasFromMap(Map<String, dynamic> item)
       : id = item["id"],
         enunciado = item["enunciado"],
         personajePath = item["personajePath"],
-        grupoId = item["grupoId"],
-        partidaId = item["partidaId"];
+        grupoId = item["grupoId"];
 
   Map<String, Object> preguntasToMap() {
     return {
       'id': id,
       'enunciado': enunciado,
       'personajePath': personajePath,
-      'grupoId': grupoId,
-      'partidaId': partidaId
+      'grupoId': grupoId
     };
   }
 
   @override
   String toString() {
     return 'Pregunta {id: $id, enunciado: $enunciado, personajePath: $personajePath, '
-        'grupoId: $grupoId, partidaId: $partidaId}';
+        'grupoId: $grupoId}';
   }
 }
