7e6e70e220295685442250afe35d60e5db9abc8b
alonsodesansegundo <alonsodesansegundo@gmail.com>
2024-04-16 11:41:59 +0200

    Creación de la pantalla de ayuda del juego Ironías

--- lib/screens/ironias/ayudaIronias.dart
+++ lib/screens/ironias/ayudaIronias.dart
@@ -25,7 +25,10 @@ class _AyudaIroniasState extends State<AyudaIronias> {
       espacioAlto,
       imgWidth,
       imgBtnWidth,
-      imgVolverHeight;
+      imgVolverHeight,
+      btnRespuestaWidth,
+      gameTextSize,
+      espacioAltoAux;
 
   late ImageTextButton btnSeguirAyuda,
       btnSalirAyudaFromHome,
@@ -117,7 +120,7 @@ class _AyudaIroniasState extends State<AyudaIronias> {
                     Expanded(
                       child: Text(
                         'Aquí descubrirás cómo jugar a \'Ironías\', '
-                        'un juego que consiste en si una situación o diálogo dado se trata de una ironía o no. '
+                        'un juego que consiste en indicar si una frase en una situación dada se trata de una ironía o no. '
                         '\nAquí tienes un ejemplo:',
                         style: TextStyle(
                           fontFamily: 'ComicNeue',
@@ -128,9 +131,161 @@ class _AyudaIroniasState extends State<AyudaIronias> {
                   ],
                 ),
                 SizedBox(height: espacioAlto),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '¡Qué alegría, que buen día!',
+                        style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: textSize,
+                        ),
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/img/ironias/sol.png',
+                      width: imgWidth * 1.3,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: btnRespuestaWidth,
+                  child: ElevatedButton(
+                    onPressed: () {},
 
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    ), // Esto desactiva el botón
+                    child: Text(
+                      'No, no es una ironía.',
+                      style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: gameTextSize,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: espacioAlto),
+                Container(
+                  width: btnRespuestaWidth,
+                  padding: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    ),
+                    child: Text(
+                      'Sí, es una ironía.',
+                      style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: gameTextSize,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: espacioAlto / 2), // Espacio entre los textos
+                Text(
+                  'Debemos de pulsar la opción que creemos que es correcta.',
+                  style: TextStyle(
+                    fontFamily: 'ComicNeue',
+                    fontSize: textSize,
+                  ),
+                ),
+                SizedBox(height: espacioAlto / 2), // Espacio entre los textos
+                Text(
+                  'Por ejemplo, si creemos que la opción correcta es \'Sí, es una ironía.\' la pulsamos. '
+                  'Una vez pulsada esta se pondrá en verde y las demás en rojo.',
+                  style: TextStyle(
+                    fontFamily: 'ComicNeue',
+                    fontSize: textSize,
+                  ),
+                ),
+                SizedBox(
+                  height: espacioAlto * 2,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '¡Qué alegría, que buen día!',
+                        style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: textSize,
+                        ),
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/img/ironias/sol.png',
+                      width: imgWidth * 1.3,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: btnRespuestaWidth,
+                  child: ElevatedButton(
+                    onPressed: () {},
+
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.redAccent,
+                    ), // Esto desactiva el botón
+                    child: Text(
+                      'No, no es una ironía.',
+                      style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: gameTextSize,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: espacioAlto),
+                Container(
+                  width: btnRespuestaWidth,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightGreen,
+                    ),
+                    child: Text(
+                      'Sí, es una ironía.',
+                      style: TextStyle(
+                          fontFamily: 'ComicNeue',
+                          fontSize: gameTextSize,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: espacioAlto / 2), // Espacio entre los textos
+                Text(
+                  'En este momento nuestra opción seleccionada es \'Sí, es una ironía.\'',
+                  style: TextStyle(
+                    fontFamily: 'ComicNeue',
+                    fontSize: textSize,
+                  ),
+                ),
+                SizedBox(height: espacioAlto / 2), // Espacio entre los textos
+                Text(
+                  'Para confirmar nuestra respuesta debemos de pulsar el botón \'Confirmar\' que se encuentra en la parte de abajo.',
+                  style: TextStyle(
+                    fontFamily: 'ComicNeue',
+                    fontSize: textSize,
+                  ),
+                ),
+                SizedBox(height: espacioAlto),
+                Text(
+                  'Esperamos que esta ayuda te haya sido de utilidad.\n¡Muchas gracias por tu atención!',
+                  style: TextStyle(
+                    fontFamily: 'ComicNeue',
+                    fontSize: textSize,
+                  ),
+                ),
                 SizedBox(height: espacioAlto * 2),
-
                 ImageTextButton(
                   image: Image.asset('assets/img/botones/fin.png',
                       width: imgWidth * 0.75),
@@ -167,11 +322,16 @@ class _AyudaIroniasState extends State<AyudaIronias> {
 
     titleSize = screenSize.width * 0.10;
     textSize = screenSize.width * 0.04;
+    gameTextSize = screenSize.width * 0.03;
+
     espacioPadding = screenSize.height * 0.03;
     espacioAlto = screenSize.height * 0.03;
+    espacioAltoAux = screenSize.height * 0.01;
+
     imgWidth = screenSize.width / 5;
     imgBtnWidth = screenSize.width / 5;
     imgVolverHeight = screenSize.height / 32;
+    btnRespuestaWidth = (screenSize.width - espacioPadding * 2) / 1.5;
   }
 
   // metodo para crear los botones necesarios en los cuadros de dialogos
@@ -260,7 +420,7 @@ class _AyudaIroniasState extends State<AyudaIronias> {
         title: '¡Genial!',
         titleSize: titleSize,
         content:
-            "Si ya estás preparado para empezar a jugar, volverás al menú principal de \'Rutinas\'.\n"
+            "Si ya estás preparado para empezar a jugar, volverás al menú principal de \'Ironías\'.\n"
             "Si todavía no te sientes preparado, no te preocupes, puedes seguir viendo la explicación de cómo jugar.",
         contentSize: textSize,
         leftImageTextButton: btnSeguirAyuda,
