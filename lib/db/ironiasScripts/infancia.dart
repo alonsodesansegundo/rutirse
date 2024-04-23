e0ba103891971cbc92dae805b3445d13cb8e55a5
alonsodesansegundo <alonsodesansegundo@gmail.com>
2024-04-23 13:29:56 +0200

    Añadidas nuevas preguntas sobre ironías al grupo de infancia

--- lib/db/ironiasScripts/infancia.dart
+++ lib/db/ironiasScripts/infancia.dart
@@ -11,7 +11,8 @@ void insertIroniasInitialDataInfancia(Database database) async {
 
   int id_P1 = await insertSituacionIroniaInitialData(
       database,
-      '¡No te vayas a desabrigar que hace frío!',
+      'Es un día de invierno, una madre ve que su hijo está a punto de salir de casa.'
+      'La madre dice: "¡No te vayas a desabrigues que hace frío!"',
       pathIronias + 'frio.png',
       grupoInfancia);
   insertRespuestaIronia(
@@ -21,7 +22,8 @@ void insertIroniasInitialDataInfancia(Database database) async {
 
   int id_P2 = await insertSituacionIroniaInitialData(
       database,
-      '¡No te vayas a desabrigar que hace frío!',
+      'Es un día de verano, un padre ve a su hija que está a punto de salir de casa.'
+      'El padre dice: "¡No te vayas a desabrigar que hace frío!"',
       pathIronias + 'calor.png',
       grupoInfancia);
   insertRespuestaIronia(
@@ -31,7 +33,8 @@ void insertIroniasInitialDataInfancia(Database database) async {
 
   int id_P3 = await insertSituacionIroniaInitialData(
       database,
-      'Un padre entra a la habitación de su hija, la cual está estudiando. El padre dice: ¡Caray, cuanto estudiamos!',
+      'Un padre entra a la habitación de su hija, la cual está estudiando. '
+      'El padre dice: "¡Caray, cuánto estudiamos!"',
       pathIronias + 'estudiar.png',
       grupoInfancia);
   insertRespuestaIronia(
@@ -47,7 +50,8 @@ void insertIroniasInitialDataInfancia(Database database) async {
 
   int id_P4 = await insertSituacionIroniaInitialData(
       database,
-      'Una madre entra a la habitación de su hijo, el cual está jugando. La madre dice: ¡Caray, cuanto estudiamos!',
+      'Una madre entra a la habitación de su hijo, el cual está jugando. '
+      'La madre dice: "¡Caray, cuánto estudiamos!"',
       pathIronias + 'jugar.png',
       grupoInfancia);
   insertRespuestaIronia(
@@ -60,4 +64,91 @@ void insertIroniasInitialDataInfancia(Database database) async {
       "Es una ironía, la madre realmente quiere decir que su hijo debería de estar estudiando.",
       1,
       id_P4);
+
+  int id_P5 = await insertSituacionIroniaInitialData(
+      database,
+      'Un padre entra en la habitación de su hija y ve todo desordenado.'
+      'El padre dice: ¡Qué desorden, dios mío!',
+      pathIronias + 'desordenado.png',
+      grupoInfancia);
+  insertRespuestaIronia(
+      database,
+      "No es una ironía, la verdad es que la habitación está ordenada.",
+      0,
+      id_P5);
+  insertRespuestaIronia(
+      database,
+      "No es una ironía, la verdad es que la habitación está desordenada.",
+      1,
+      id_P5);
+
+  int id_P6 = await insertSituacionIroniaInitialData(
+      database,
+      'A una persona se le cae un jarrón, con la suerte de que el jarrón no se rompe. '
+      'La persona al ver el jarrón intacto dice: "Uf, ¡qué buena suerte he tenido!"',
+      pathIronias + 'jarron.png',
+      grupoInfancia);
+  insertRespuestaIronia(
+      database, "No es una ironía, realmente ha tenido mala suerte.", 0, id_P6);
+  insertRespuestaIronia(database,
+      "No es una ironía, realmente ha tenido buena suerte.", 1, id_P6);
+
+  int id_P7 = await insertSituacionIroniaInitialData(
+      database,
+      'A una persona se le cae un jarrón, con tan mala suerte que el jarrón se rompe. '
+      'La persona al ver el jarrón roto dice: "Caray, ¡qué buena suerte la mía!"',
+      pathIronias + 'jarronroto.png',
+      grupoInfancia);
+  insertRespuestaIronia(database,
+      "No es una ironía, realmente ha tenido buena suerte.", 0, id_P7);
+  insertRespuestaIronia(
+      database, "Es una ironía, realmente ha tenido mala suerte.", 1, id_P7);
+
+  int id_P8 = await insertSituacionIroniaInitialData(
+      database,
+      'Pepe ha visto una película de miedo por la tarde y ahora se encuentra en cama sin ser capaz de dormir.'
+      'Pepe piensa: "No debí ver esa película de miedo".',
+      pathIronias + 'despierto.png',
+      grupoInfancia);
+  insertRespuestaIronia(
+      database,
+      "No es una ironía, ver esa película de miedo no fue una buena idea.",
+      1,
+      id_P8);
+  insertRespuestaIronia(database,
+      "Es una ironía, ver esa película de miedo fue una buena idea.", 0, id_P8);
+
+  int id_P9 = await insertSituacionIroniaInitialData(
+      database,
+      'María ha visto una película de miedo por la tarde y ahora se encuentra en cama sin ser capaz de dormir.'
+      'María piensa: "¡Qué buena idea ver esa película de miedo!".',
+      pathIronias + 'despierto.png',
+      grupoInfancia);
+  insertRespuestaIronia(
+      database,
+      "No es una ironía, ver esa película de miedo fue una buena idea.",
+      0,
+      id_P9);
+  insertRespuestaIronia(
+      database,
+      "Es una ironía, ver esa película de miedo no fue una buena idea.",
+      1,
+      id_P9);
+
+  int id_P10 = await insertSituacionIroniaInitialData(
+      database,
+      'Una madre entra en la habitación de su hijo y ve todo desordenado.'
+      'La madre dice: ¡Qué ordenado está todo!',
+      pathIronias + 'desordenado.png',
+      grupoInfancia);
+  insertRespuestaIronia(
+      database,
+      "No es una ironía, la verdad es que la habitación está ordenada.",
+      0,
+      id_P10);
+  insertRespuestaIronia(
+      database,
+      "Es una ironía, la verdad es que la habitación está desordenada.",
+      1,
+      id_P10);
 }
